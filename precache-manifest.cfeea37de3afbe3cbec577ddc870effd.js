b614773dbe6158d27bd6b626ed32232e98f7e289
Austin Atwood <austin.w.atwood@gmail.com>
2020-04-09 23:08:31 +0900

    Updates

---
+++ precache-manifest.cfeea37de3afbe3cbec577ddc870effd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9a0aedbb300244d4a962bd58f33748",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "756cb910cfb905ddbb82",
+    "url": "/monsters-rolodex/static/css/main.4f5e91c8.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756cb910cfb905ddbb82",
+    "url": "/monsters-rolodex/static/js/main.4c5120da.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
